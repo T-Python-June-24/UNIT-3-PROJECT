4ee437176ec7740873c21d66a80da5b8647f0f42
Sarah <monifsarah@gmail.com>
2024-07-26 14:16:16 +0300

    added main functionaltis of Category

--- IventoryPlus/Category/urls.py
+++ IventoryPlus/Category/urls.py
@@ -13,8 +13,5 @@ urlpatterns = [
     path('update/<category_id>/', views.category_update, name='category_update'),    
     path("delete/<category_id>/", views.delete_category, name="delete_category"),
 
-    # path("update/<game_id>/", views.game_update_view, name="game_update_view"),
-    # path("search/", views.search_games_view, name="search_games_view"),
-    # path("<category_name>/", views.all_games_view, name="all_games_view"),
-    # path("reviews/add/<game_id>/", views.add_review_view, name="add_review_view")
+
 ]
