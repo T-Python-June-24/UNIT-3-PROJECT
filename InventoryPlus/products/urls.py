ec6f52705e9c6929d974d2ca2efe1510b9b447ce
bushraali98 <bm-bisho-90210@hotmail.com>
2024-07-28 13:14:13 +0300

    added product details and search

--- InventoryPlus/products/urls.py
+++ InventoryPlus/products/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
   path("all/", views.products_view, name="products_view"),
   path("add/", views.add_product, name="add_product"),
   path("edit/<int:product_id>/", views.edit_product_view, name="edit_product_view"),
-  path('delete/<int:product_id>', views.delete_product_view, name="delete_product_view")
-
+  path('delete/<int:product_id>/', views.delete_product_view, name="delete_product_view"),
+  path('detail/<int:product_id>/', views.product_detail_view, name="product_detail_view"),
+  path('search/', views.product_search_view, name="product_search_view")
 ]
--- InventoryPlus/products/views.py
+++ InventoryPlus/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
-from .forms import ProductForm
 from .models import Product
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,35 +13,55 @@ def products_view(request: HttpRequest):
 
 def add_product(request: HttpRequest):
   if request.method == 'POST':
-    form = ProductForm(request.POST)
-    if form.is_valid():
-      form.save()
-      return redirect("products:products_view")
-  else:
-    form = ProductForm
+    name = request.POST.get('name')
+    image = request.FILES.get('image')
+    description = request.POST.get('description')
+    price = request.POST.get('price')
+    stock_quantity = request.POST.get('stock_quantity')
 
-  return render(request, "products/add_product.html", {'form': form})
+    product = Product(name=name, image=image, description=description, price=price, stock_quantity=stock_quantity)
+    product.save()
+
+    return redirect("products:products_view")
+  
+  return render(request, "products/add_product.html")
 
 
 
 
 def edit_product_view(request: HttpRequest, product_id):
   product = Product.objects.get(pk=product_id)
-  if request.method == 'POST':
-    form = ProductForm(request.POST, request.FILES, instance=product)
-    if form.is_valid():
-      form.save()
-      return redirect("products:products_view")
-  else:
-    form = ProductForm(instance=product)
   
-  return render(request, "products/edit_product.html", {'form': form, 'product': product})
+  if request.method == 'POST':
+    product.name = request.POST.get('name')
+    product.image = request.FILES.get('image', product.image)
+    product.description = request.POST.get('description')
+    product.price = request.POST.get('price')
+    product.stock_quantity = request.POST.get('stock_quantity')
+    product.save()
+    return redirect("products:edit_product_view", product_id=product_id)
+  
+  return render(request, "products/edit_product.html", {'product': product})
 
 
 
 def delete_product_view(request: HttpRequest, product_id):
   product = Product.objects.get(pk=product_id)
-  if request.method == "POST":
-    product.delete()
-    return redirect("products:products_view")
-  return render(request, "delete_product.html", {'product': product})
+  product.delete()
+  return redirect("products:products_view")
+
+
+def product_detail_view(request: HttpRequest, product_id):
+  product = Product.objects.get(pk = product_id)
+  return render(request, "products/product_details.html", {'product': product})
+
+def product_search_view(request: HttpRequest):
+  query = request.GET.get('q')
+  if query:
+    products = Product.objects.filter(
+      Q(name__icontains=query) |
+      Q(description__icontains=query)
+    )
+  else:
+    products = Product.objects.all()
+  return render(request, "products/products.html", {'products': products})
