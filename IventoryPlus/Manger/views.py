571cc081f38d73ee42504f0d44c15a2936cdac3a
naif <naif.n1158@gmail.com>
2024-07-28 21:44:29 +0300

    The Dashboard page has been completed and work is underway on the rest of the Manager page

--- IventoryPlus/Manger/views.py
+++ IventoryPlus/Manger/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from product.models import Product
+from Category.models import Category
+from Supplier.models import Supplier
+import plotly.express as px
+import pandas
 # Create your views here.
 
 
 def Manger(request:HttpRequest):
-    return render(request , 'pages/pages_manger.html')
+    Quantity_product = Product.objects.filter(Quantity_Product__lt = 2).exclude(Quantity_Product=0)
+    out_product = Product.objects.filter(Status_Product = 0)
+    update_product = Product.objects.all().order_by('-Created_at')
+    number_product = Product.objects.count()
+    number_Supplier = Supplier.objects.count()
+    number_Category = Category.objects.count()
+
+    return render(request , 'pages/pages_manger.html' ,
+                  {"Quantity_product":Quantity_product ,
+                   "out_product":out_product , 
+                   "update_product":update_product,
+                   "number_product":number_product,
+                   "number_supplier":number_Supplier,
+                   "number_category":number_Category
+                   })
