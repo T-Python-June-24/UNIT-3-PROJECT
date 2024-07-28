50a1e6aaef64860ca88d31116e4cba946dbdfca3
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-28 17:50:29 +0300

    fix the realtionship between products and suppliers

--- InventoryPlus/product/models.py
+++ InventoryPlus/product/models.py
@@ -4,7 +4,7 @@ from supplier.models import Supplier
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE ,related_name='products')
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='products')
+    suppliers = models.ManyToManyField(Supplier, related_name='products')
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
