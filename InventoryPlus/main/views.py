6ea3416034cb4059f802b41b65e0422374ce95ec
D. F <ifonei@hotmail.com>
2024-07-30 00:23:42 +0300

    adding admin dashboard

--- InventoryPlus/main/views.py
+++ InventoryPlus/main/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render,redirect
 from django.http import HttpRequest,HttpResponse
-from products.models import Product
+from products.models import Product,Supplier,Category
+from datetime import datetime,timedelta
 # Create your views here.
 
 
 
 def home_view(request:HttpRequest):
 
-    #get all products
-    products = Product.objects.all().order_by("product_name")[0:3]
+    products = Product.objects.all().order_by("product_name")
+    suppliers = Supplier.objects.all().order_by("supplier_name")
+    categories = Category.objects.all().order_by("name")
 
-    return render(request, 'main/index.html', {"products" : products} )
+    # Calculate the date 10 days from today
+    ten_days_from_today = datetime.now().date()+ timedelta(days=10)
+    # Filter products with expiry_date less than or equal to 10 days from today
+    ex_products = Product.objects.filter(expiry_date__lte=ten_days_from_today).order_by('product_name')
+
+    return render(request, 'main/index.html', {"products" : products,"suppliers": suppliers, 'categories': categories, 'ex_products':ex_products})
 
 
 def contact_view(request:HttpRequest):
