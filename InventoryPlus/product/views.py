881d7a33c349f2c14b84805d3f24a81367f1c552
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-30 10:55:57 +0300

    fix some bugs in product app

--- InventoryPlus/product/views.py
+++ InventoryPlus/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest,HttpResponse
-from product.models import Product
-from supplier.models import Supplier
+from .models import Product,Supplier , Category
+
 
 
 def all_products_view(request:HttpRequest ):
@@ -15,17 +15,22 @@ def delete_products_view(request:HttpRequest, product_id: int):
 
 def add_products_view(request: HttpRequest):
     products = Product.objects.all()
+    category = Category.objects.all()
+    allSuppliers = Supplier.objects.all() 
     if request.method == "POST":
+        category_id = request.POST['category']
+        category_instance = Category.objects.get(id=category_id)
         new_product = Product(name=request.POST['name'],
                                 description=request.POST['description'],
                                 price=request.POST['price'],
-                                category=request.POST['category'],
+                                category= category_instance,
                                 image= request.FILES['image'])
         new_product.save()
         supplier_ids = request.POST.getlist('suppliers')
         suppliers = Supplier.objects.filter(id__in=supplier_ids)
         new_product.suppliers.add(*suppliers)
-    return render(request, "addProducts.html",  {"products": products})
+
+    return render(request, "addProducts.html",  {"products": products,"categories":category,"suppliers": allSuppliers})
 
 def update_products_view(request:HttpRequest, product_id:int):
     products = Product.objects.get(id=product_id)
