85d12a5807ede74362d57190dbc46610ac79b35e
amira alrshidi <ameerah.alrashidi0111@gmail.com>
2024-08-03 19:04:14 +0300

    edit product models and templates

--- InventoryPlus/products/models.py
+++ InventoryPlus/products/models.py
@@ -7,16 +7,16 @@ from categories.models import Category
 class Product(models.Model):
     productName = models.CharField(max_length=128)
     productDescription = models.TextField()
-    productImage = models.ImageField(upload_to='images/')
     productPrice = models.FloatField(default=0) # يحدده البائع 
-    productionDate = models.DateField() 
+    # productionDate = models.DateField() 
     expirationDate = models.DateField()
     productStock = models.IntegerField(default=0) #بالبدايه راح تتعبى القيمه من السبلايرز في صفحة تسجيل المنتج 
     unitPrice = models.FloatField(default=0.0)
     totalPriceOfunits = models.FloatField(default=0.0)
-    supplierDiscount = models.SmallIntegerField(default=0)
+    # supplierDiscount = models.SmallIntegerField(default=0)
     productCategory = models.ForeignKey(Category, on_delete=models.PROTECT,null=True) # المفترض تكون سيلكت اوبشن ويختار منها المدير 
     productSupplier =  models.ManyToManyField(Supplier) # should be select options 
+    productImage = models.ImageField(upload_to='images/')
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- InventoryPlus/products/views.py
+++ InventoryPlus/products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category, Supplier
 from .forms import ProductForm
 from django.core.paginator import Paginator
+from django.contrib import messages
 def all_products(request):
     query = request.GET.get('q')
     if query:
@@ -12,7 +13,7 @@ def all_products(request):
     categories = Category.objects.all()
     suppliers = Supplier.objects.all()
     form = ProductForm()
-    p = Paginator(products,1)
+    p = Paginator(products,10)
     page = request.GET.get("page")
     product_page = p.get_page(page)
     return render(request, 'products/all_products.html', {
@@ -28,25 +29,50 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', {'product': product})
 
 def product_create(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('products:all_products')
+    try:
+
+        if request.method == 'POST':
+            print(request.POST['productName'])
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Added product successfully", "success")
+                return redirect('products:all_products')
+            else:
+                print(form.errors.as_text)
+                messages.error(request, "Unable to add product. Please enter valid information", "danger")
+
+    except Exception as e :
+        print("Error: ",e)
+        messages.error(request, "Couldn't Add product", "danger")
+
     return redirect('products:all_products')
 
 def product_update(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES, instance=product)
-        if form.is_valid():
-            form.save()
-            return redirect('products:all_products')
+    try:
+
+        product = Product.objects.get(pk=product_id)
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES, instance=product)
+            if form.is_valid():
+                form.save()
+                return redirect('products:all_products')
+            else:
+                messages.error(request, "Unable to update product. Please enter valid information", "danger")
+
+    except Exception as e :
+        print("Error: ",e)
+        messages.error(request, e , "danger")
     return redirect('products:all_products')
 
 def product_delete(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    if request.method == 'POST':
-        product.delete()
+    try:
+        product = Product.objects.get(pk=product_id)
+        print(product.delete())
+        messages.success(request, "Deleted product successfully", "success")
+# add some conditions to handel certain errors 
         return redirect('products:all_products')
+    except Exception as e :
+        print("Error: ",e)
+        messages.error(request, e , "danger")
     return redirect('products:all_products')
