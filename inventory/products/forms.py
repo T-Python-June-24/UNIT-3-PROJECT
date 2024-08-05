ac671302afeefb51531c94d51d9dfb53f7706316
Abdullah Aldhamen <a.h.aldhamen@hotmail.com>
2024-08-05 21:09:28 +0300

    some edits

--- inventory/products/forms.py
+++ inventory/products/forms.py
@@ -1,15 +1,3 @@
-# from django import forms
-# from products.models import Product
-
-# # Create the form class.
-# class ProductForm(forms.ModelForm):
-#     class Meta:
-#         model = Product
-#         fields = "__all__"
-#         widgets = {
-#             'title' : forms.TextInput({"class" : "form-control"})
-#         }
-
 from django import forms
 from products.models import Product
 
