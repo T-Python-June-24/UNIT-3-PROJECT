3f24190647e253949646f60bc7f0036ad7cfc561
bushraali98 <bm-bisho-90210@hotmail.com>
2024-08-05 11:52:41 +0300

    loading the .env and updating the readme file

--- InventoryPlus/InventoryPlus/settings.py
+++ InventoryPlus/InventoryPlus/settings.py
@@ -141,6 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'  
--- InventoryPlus/products/signals.py
+++ InventoryPlus/products/signals.py
@@ -4,31 +4,35 @@ from django.core.mail import send_mail
 from .models import Product
 from datetime import datetime, timedelta
 
+
+# sending alerts to users using signals
 @receiver(post_save, sender=Product)
 def check_inventory(sender, instance, **kwargs):
   # Low stock alert
 
-  # stock_quantity = int(instance.stock_quantity)
+  stock_quantity = int(instance.stock_quantity)
 
-  # low_stock = 10
-  # if stock_quantity <= low_stock:
-  #   send_mail(
-  #     'Low Stock Alert',
-  #     f'The product {instance.name} is running low on stock (Stock: {instance.stock_quantity}).',
-  #     'inventoryplus051@hotmail.com',
-  #     ['boshraalija@gmail.com'],
-  #     fail_silently=False,
-  #   )
+  low_stock = 5
+  if stock_quantity <= low_stock:
+    send_mail(
+      'Low Stock Alert',
+      f'The product {instance.name} is running low on stock (Stock: {instance.stock_quantity}).',
+      'inventoryplus051@hotmail.com',
+      ['boshraalija@gmail.com'],
+      fail_silently=False,
+    )
 
-    # # Expiry date alert
-    # today = datetime.now().date()
-    # expiry_date = today + timedelta(days=30)
-    # if instance.expiry_date and instance.expiry_date <= expiry_date:
-    #     send_mail(
-    #         'Expiry Date Alert',
-    #         f'The product {instance.name} is nearing its expiry date (Expiry Date: {instance.expiry_date}).',
-    #         'inventoryplus051@hotmail.com',
-    #         ['boshraalija@gmail.com'],
-    #         fail_silently=False,
-    #     )
-  pass
+    # Expiry date alert
+    # get the current date
+    today = datetime.now().date()
+    # expiration date after 30 days
+    expiry_date = today + timedelta(days=30)
+    # Checking if the product has an expiry date and if it is within 30 days.
+    if instance.expiry_date and instance.expiry_date <= expiry_date:
+        send_mail(
+            'Expiry Date Alert',
+            f'The product {instance.name} is nearing its expiry date (Expiry Date: {instance.expiry_date}).',
+            'inventoryplus051@hotmail.com',
+            ['boshraalija@gmail.com'],
+            fail_silently=False,
+        )
